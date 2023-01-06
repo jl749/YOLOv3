f187f1d311fa197643cba1c1f6f080938b2475b9
jlee <jlee@clika.io>
2023-01-07 00:32:10 +0900

    mAP func vectorization + resolve bug #6

--- yolov3/utils/metric.py
+++ yolov3/utils/metric.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from itertools import chain
 from typing import List
 
@@ -59,70 +58,73 @@ def mean_average_precision(pred_boxes: List[torch.Tensor],
     """
     calculate mAP
     :param pred_boxes: predicted bboxes [[class, conf, x, y, x, y], ...]
-    :param true_boxes: expected bboxes  [[train_idx, class_pred, obj_prob, x, y, x, y], ...]
+    :param true_boxes: expected bboxes  [[x, y, x, y, class], ...]
     :param num_classes: pascal_voc=20, coco=80
     :param iou_threshold: iou_threshold to determine TP, FP
     """
     _device = pred_boxes[0].device
-
     mAPs_per_class = [0] * num_classes
     recalls_per_class = [0] * num_classes
     precisions_per_class = [0] * num_classes
 
-    if all([True if tensor.nelement() == 0 else False for tensor in pred_boxes]):  # no predictions
-        return mAPs_per_class, recalls_per_class, precisions_per_class
+    def _unique(x, dim=-1):  # return firstly appeared unique elements' indices
+        unique, inverse = torch.unique(x, return_inverse=True, dim=dim)
+        perm = torch.arange(inverse.size(dim), dtype=inverse.dtype, device=inverse.device)
+        inverse, perm = inverse.flip([dim]), perm.flip([dim])
+        return unique, inverse.new_empty(unique.size(dim)).scatter_(dim, inverse, perm)
 
-    pred_boxes = [torch.cat([torch.tensor(i, device=_device).repeat(p.shape[0], 1), p], dim=1) for i, p in enumerate(pred_boxes)]  # prepend prediction index
-    detections = torch.stack(list(chain.from_iterable(pred_boxes)))
-    detections = detections[torch.argsort(detections[:, 2], descending=True)]  # sort by conf (descending)
+    stats = []
+    for p, t in zip(pred_boxes, true_boxes):
+        t = t.to(_device)
+        iou_matrix = torchvision.ops.box_iou(boxes1=t[:, 0:4], boxes2=p[:, 2:6])  # (num_labels, num_preds)
+        _filtered_indexes = torch.where(
+            torch.bitwise_and(iou_matrix > iou_threshold, t[:, -1:] == p[:, 0])
+        )
+        _TP = torch.zeros(p.shape[0], dtype=torch.bool, device=_device)
+        if _filtered_indexes[0].shape[0]:  # if match exists
+            matches = torch.cat([torch.stack(_filtered_indexes, 1),
+                                 iou_matrix[_filtered_indexes].unsqueeze(-1)], dim=1)
+            """ matches: (i, j, iou_corresponding_to_index_ij) """
 
-    ground_truths = torch.stack(list(chain.from_iterable(true_boxes)))
+            # match label -- detection (one2one)
+            matches = matches[torch.argsort(matches[:, 2], descending=True)]  # sort by ious
+            matches = matches[_unique(matches[:, 1])[1]]  # sort by detection index (if dup keep first observed, drop rest)
+            matches = matches[_unique(matches[:, 0])[1]]  # sort by label index  (if dup keep first observed, drop rest)
+            _TP[matches[:, 1].long()] = True  # TODO: matches[:, 2] > iou_thresh for multiple iou_thresh
+        stats.append((_TP, p[:, 1], p[:, 0], t[:, -1]))
+        """
+        stats = [(TP, conf, pcls, tcls), ...]
+        - TP = (num_detection,)  # detection_ious > iou_threshold
+        - conf = (num_detection,)  # detection confidence
+        - pcls = (num_detection,)  # predicted classes
+        - tcls = (num_targets,)  # target classes
+        """
+    stats = [torch.cat(x, 0) for x in zip(*stats)]
+    TP, _pred_conf, pred_cls, target_cls = stats
+    _sorted_index = torch.argsort(_pred_conf, descending=True)
+    TP = TP[_sorted_index]
+    pred_cls = pred_cls[_sorted_index]
 
-    for c in range(num_classes):
-        # filter by class
-        detections_c = detections[detections[:, 1] == c]
-        ground_truths_c = ground_truths[ground_truths[:, -1] == c]
+    unique_cls, target_cls_counts = torch.unique(target_cls, return_counts=True)
+    for i, c in enumerate(unique_cls):
+        _sorted_index_c = pred_cls == c  # boolean index
+        total_labels: int = target_cls_counts[i]  # TP + FN
+        total_preds: int = torch.sum(_sorted_index_c)  # TP + FP
+        _TP_c = TP[_sorted_index_c]
 
-        _label_counts_per_img = [torch.zeros(tb.shape[0], dtype=torch.bool) for tb in true_boxes]
-        TP = torch.zeros((len(detections_c)), dtype=torch.bool)
+        if total_labels == 0 or total_preds == 0:
+            continue
+        TP_cumsum = torch.cumsum(TP[_sorted_index_c], dim=0)
+        FP_cumsum = torch.cumsum(~TP[_sorted_index_c], dim=0)
 
-        for i, pred in enumerate(detections_c):  # for a single bbox (high conf --> low conf)
-            _img_idx = pred[0].long()
-            labels = true_boxes[_img_idx].to(_device)  # compare labels and detections from the same img
-            if labels.shape[0] == 0:  # empty label
-                continue
-
-            # find best matching GT label from iou_matrix
-            iou_matrix = torchvision.ops.box_iou(boxes1=labels[:, 0:4], boxes2=pred[3:7].unsqueeze(0))
-            max_overlap, max_idx = torch.max(iou_matrix, dim=0)  # (label_count, pred_count) --> (pred_count,), pred_count is always 1
-
-            if max_overlap.gt(iou_threshold) and not _label_counts_per_img[_img_idx][max_idx.item()].is_nonzero():
-                TP[i] = True
-                _label_counts_per_img[_img_idx][max_idx.item()] = True  # this GT label has been used
-
-        TP_cumsum = torch.cumsum(TP, dim=0)
-        FP_cumsum = torch.cumsum(~TP, dim=0)
-        _recalls = TP_cumsum / ground_truths_c.shape[0]  # TP_cumsum / total_GT_boxes
+        _recalls = TP_cumsum / (total_labels + 1e-16)  # TP_cumsum / total_GT_boxes (TP + FN)
         _precisions = TP_cumsum / (TP_cumsum + FP_cumsum)  # TP_cumsum / TP + FP
-        _precisions = torch.cat([torch.tensor([1]), _precisions])  # torch.tensor([0])
-        _recalls = torch.cat([torch.tensor([0]), _recalls])  # _recalls[-2:-1]
-
-        # DBUGGING =====================================================================================================
-        # import matplotlib.pyplot as plt
-        # plt.plot(_recalls, _precisions, c="blue")
-        # plt.xlabel("recall")
-        # plt.ylabel("precision")
-        # plt.title(f"mAP_{iou_threshold}")
-        # plt.grid(color="gray")
-        # plt.show()
-        # plt.close()
-        # ==============================================================================================================
+        _precisions = torch.cat([torch.tensor([1], device=_device), _precisions, torch.tensor([0], device=_device)])
+        _recalls = torch.cat([torch.tensor([0], device=_device), _recalls, torch.tensor([1], device=_device)])
 
         mAP = torch.trapz(y=_precisions, x=_recalls)
-        mAPs_per_class[c] = mAP.item()
-
-        # TODO: make sure
-        precisions_per_class[c] = TP.sum().item() / TP.shape[0]  # TP / TP + FP
-        recalls_per_class[c] = TP.sum().item() / ground_truths_c.shape[0]  # TP / TP + FN
+        mAPs_per_class[c.long()] = mAP.item()
+        recalls_per_class[c.long()] = _TP_c.sum().item() / total_labels  # TP / TP + FN
+        precisions_per_class[c.long()] = _TP_c.sum().item() / total_preds  # TP / TP + FP
 
     return mAPs_per_class, recalls_per_class, precisions_per_class
